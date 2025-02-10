b9dbab0597afafae33902026355cef0cdf475a87
marlhey <143136080+marlhey@users.noreply.github.com>
2025-02-10 12:32:00 -0600

    Dashboard

--- api/routes/web.php
+++ api/routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin',function(){
+    return view('admin.index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
