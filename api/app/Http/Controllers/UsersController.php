1d20d3a3eca0e73c0cd7a2d7cc12d7911371f98f
MelissaGamerosT <143050843+MelissaGamerosT@users.noreply.github.com>
2025-02-24 12:21:17 -0600

    email verificacion admin

---
+++ api/app/Http/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+class UsersController extends Controller
+{
+    public function sendEmail(){
+        $data['code']=rand(1000,9999);
+        $to="pinamaricruz858@gmail.com"; //destino email del usuario
+        $subject="Código de verificación";//asunto de correo
+        Mail::send('mails.verification',$data,function($message) use ($to,$subject){
+                $message->to($to,'Mi tienda online')
+                ->subject($subject);
+                $message->from('americamgd22@gmail.com',"Bienvenido a mi tienda Online");
+                        });
+        echo"Sen envió el correo";
+    }
+}
--- api/routes/web.php
+++ api/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/send-email',[UsersController::class,'sendEmail']);
 Route::get('/admin',function(){
     return view('admin.index');
 });
