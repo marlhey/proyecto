7f925a26f133126b1fcd8e978471480f4b0c7bdc
marlhey <143136080+marlhey@users.noreply.github.com>
2025-03-21 11:20:35 -0600

    header Mari

--- mmpgstore_u/src/views/home/HomeComponent.vue
+++ mmpgstore_u/src/views/home/HomeComponent.vue
@@ -6,43 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="index.html" class="logo" >
-                            <img src="@/template/assets/images/logommpg.jpg" style="width: 100px; height: 100px;">
-                        </a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                            <li class="scroll-to-section"><a href="#men">Men's</a></li>
-                            <li class="scroll-to-section"><a href="#women">Women's</a></li>
-                            <li class="scroll-to-section"><a href="#kids">Kid's</a></li>
-                            <li class="submenu">
-                                <a href="javascript:;">Pages</a>
-                                <ul>
-                                    <li><a href="about.html">About Us</a></li>
-                                    <li><a href="products.html">Products</a></li>
-                                    <li><a href="single-product.html">Single Product</a></li>
-                                    <li><a href="contact.html">Contact Us</a></li>
-                                </ul>
-                            </li>
-                            <li class="submenu">
-                                <a href="javascript:;">Features</a>
-                                <ul>
-                                    <li><a href="#">Features Page 1</a></li>
-                                    <li><a href="#">Features Page 2</a></li>
-                                    <li><a href="#">Features Page 3</a></li>
-                                    <li><a rel="nofollow" href="https://templatemo.com/page/4" target="_blank">Template Page 4</a></li>
-                                </ul>
-                            </li>
-                            <li class="scroll-to-section"><a href="#explore">Explore</a></li>
-                        </ul>        
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
+                    <HeaderComponent/>
                 </div>
             </div>
         </div>
@@ -762,7 +726,12 @@
 </template>
 
 <script>
+import HeaderComponent from '@/shared/HeaderComponent.vue';
+
 export default{
     name: 'HomeComponent',
+    components:{
+        HeaderComponent
+    }
 }
 </script>
