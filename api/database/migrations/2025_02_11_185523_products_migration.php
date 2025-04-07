15cb3a352b750ecd227b90a2eec8036f26e7b46d
marlhey <143136080+marlhey@users.noreply.github.com>
2025-04-07 12:18:38 -0600

    cambio de tamaño del nombre Mari

--- api/database/migrations/2025_02_11_185523_products_migration.php
+++ api/database/migrations/2025_02_11_185523_products_migration.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table){
             $table->increments('id');
-            $table->string('name',30);
+            $table->string('name',100);
             $table->string('slug',100);
             $table->string('img');
             $table->double('price');
